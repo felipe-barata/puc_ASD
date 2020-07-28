77bf7f5f3bc48c40367ed989fd6d1d37410840e2
felipe-barata <felipeabarata@gmail.com>
2020-07-28 13:27:51 -0300

    incluir perfil de testes que faltou em uma classe

--- normas/src/test/java/br/com/sigo/normas/NormasApplicationTests.java
+++ normas/src/test/java/br/com/sigo/normas/NormasApplicationTests.java
@@ -2,12 +2,14 @@ package br.com.sigo.normas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NormasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
